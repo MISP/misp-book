39feb95d48c4670d52947bae46774346c4ed88d7
Alexandre Dulaunoy <a@foo.be>
2018-11-01 16:30:38 +0100

    chg: [publish] ensure permission of output is correct

--- publish.sh
+++ publish.sh
@@ -6,5 +6,6 @@ gitbook mobi
 mv book.pdf _book
 mv book.epub _book
 mv book.mobi _book
+chmod +r _book/book.pdf _book/book.epub _book/book.mobi
 cd _book
 rsync -azv . circl@cpab:/var/www/nwww.circl.lu/doc/misp/ && rm -rf _book
